10db82d7347324c911c3e0eefc72e9ffdae3660b
ssuchir <ssuchir@gmail.com>
2020-01-18 23:46:15 -0500

    Added Loading screen and CSS

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,17 @@
+import './SeasonDisplay.css'
 import React from 'react';
+
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "Brr it's cold!",
+    iconName: 'snowflake'
+  },
+};
 
 const getSeason = ( lat, month) => {
   if (month > 2 && month < 9) {
@@ -9,15 +22,14 @@ const getSeason = ( lat, month) => {
 };
 
 const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'winter' ? 'Brr, it is chilly' : 'Lets hit the beach!'
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${icon} icon`} />
+    <div className ={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
 );
 };
